ab26d44549f1f6681f77049a8aa8d5b479a26c70
cok263 <cok26385@gmail.com>
2020-11-13 12:39:47 +0300

    refactoring code...

--- main.py
+++ main.py
@@ -7,42 +7,40 @@ from dotenv import load_dotenv
 
 
 def download_img(url, filename):
-    response = requests.get(url)
+    response = requests.get(url, verify=False)
     response.raise_for_status()
     os.makedirs(os.path.dirname(filename), exist_ok=True)
     with open(filename, 'wb') as file:
         file.write(response.content)
-    resizeImage(filename)
+    resize_image(filename)
 
 
-def fetch_spacex_last_launch(launch_num=-1):
+def fetch_spacex_launch(launch_num=-1):
     url = 'https://api.spacexdata.com/v4/launches/'
     if launch_num == -1:
         url = urljoin(url, 'latest')
     response = requests.get(url)
     response.raise_for_status()
-    answer = response.json()
+    launches = response.json()
+    launch = launches
     if launch_num is not -1:
-        answer = answer[launch_num]
-    images = answer['links']['flickr']['original']
+        launch = launches[launch_num]
+    images = launch['links']['flickr']['original']
     for number, image_link in enumerate(images):
         download_img(image_link, 'images/spacex{}.jpg'.format(number))
-
-
-def get_extension(url):
-    return url.split('/')[-1].split('.')[-1]
 
 
 def download_hubble_image(id):
     url = 'http://hubblesite.org/api/v3/image/{}'.format(id)
     response = requests.get(url)
     response.raise_for_status()
-    answer = response.json()
-    images = [img['file_url'].replace(
-        '//imgsrc.hubblesite.org/hvi', 'https://media.stsci.edu'
-        ) for img in answer['image_files']]
-    img_link = images[-1]
-    download_img(img_link, 'images/{}.{}'.format(id, get_extension(img_link)))
+    image_details = response.json()
+    images = [img['file_url'] for img in image_details['image_files']]
+    img_link = 'http:{}'.format(images[-1])
+    download_img(
+        img_link,
+        'images/{id}{exp}'.format(id=id, exp=os.path.splitext(img_link)[-1])
+    )
 
 
 def download_hubble_image_collection():
@@ -54,14 +52,14 @@ def download_hubble_image_collection():
     url = 'http://hubblesite.org/api/v3/images/'
     response = requests.get(url, params=payload)
     response.raise_for_status()
-    answer = response.json()
-    images_id = [info['id'] for info in answer]
-    for id in images_id:
-        print('downloading image id={}...'.format(id))
-        download_hubble_image(id)
+    images_collection = response.json()
+    images_id = [info['id'] for info in images_collection]
+    for image_id in images_id:
+        print('downloading image id={}...'.format(image_id))
+        download_hubble_image(image_id)
 
 
-def resizeImage(img_src):
+def resize_image(img_src):
     image = Image.open(img_src)
     image.thumbnail((1080, 1080))
     image.save(img_src)
@@ -70,8 +68,8 @@ def resizeImage(img_src):
 def upload_to_instagram(login, password, images_dir):
     bot = Bot()
     bot.login(username=login, password=password)
-    images_list = os.listdir(images_dir)
-    for image in images_list:
+    images = os.listdir(images_dir)
+    for image in images:
         bot.upload_photo(os.path.join(images_dir, image), caption="Nice pic!")
 
 
@@ -79,9 +77,10 @@ def main():
     load_dotenv()
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
-    fetch_spacex_last_launch(12)
+    spacex_launch_id = 12
+    fetch_spacex_launch(spacex_launch_id)
     download_hubble_image_collection()
-    upload_to_instagram(login, password, 'images')
+    #upload_to_instagram(login, password, 'images')
 
 
 if __name__ == '__main__':
