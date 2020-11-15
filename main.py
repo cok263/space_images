0d5db67961f03dbabba965e61f25fcd370ee062e
cok263 <cok26385@gmail.com>
2020-11-15 03:42:13 +0300

    add arg images_dir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import argparse
 from urllib.parse import urljoin
 from dotenv import load_dotenv
 from PIL import Image
@@ -9,14 +10,26 @@ from hubble import download_hubble_image_collection
 from instagram_tools import upload_to_instagram
 
 
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description='Программа скачивает космические \
+                     фото и загружает в инстаграмм'
+    )
+    parser.add_argument('--images_dir', help='Папка с фотографиями',
+                        default='images')
+    return parser
+
+
 def main():
+    parser = create_parser()
+    args = parser.parse_args()
     load_dotenv()
     login = os.getenv('LOGIN')
     password = os.getenv('PASSWORD')
     spacex_launch_id = 12
     fetch_spacex_launch(spacex_launch_id)
     download_hubble_image_collection()
-    upload_to_instagram(login, password, 'images')
+    upload_to_instagram(login, password, args.images_dir)
 
 
 if __name__ == '__main__':
